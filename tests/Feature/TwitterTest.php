bac26642b9acff70d2b8cb1bc6a32f61879f6a26
luiswback <luiswback@gmail.com>
2025-02-13 02:09:55 +0000

    planning tweet feature

---
+++ tests/Feature/TwitterTest.php
@@ -0,0 +1,9 @@
+<?php
+
+todo('should be able to create a tweet');
+
+todo('the tweet body should have a max length of 140 characters');
+
+todo('should show the tweet on the timeline');
+
+todo('body is required');
