60cf026bc356a68401f1b6fe3e96faae42819ece
luiswback <luiswback@gmail.com>
2025-02-12 23:51:16 -0300

    should be able to create a tweet

---
+++ app/Models/Tweet.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Tweet extends Model
+{
+    /** @use HasFactory<\Database\Factories\TweetFactory> */
+    use HasFactory;
+
+    /**
+     * @var Tweet|mixed|\Pest\Expectation|\Pest\Expectations\EachExpectation|\Pest\Expectations\HigherOrderExpectation|\Pest\Expectations\OppositeExpectation|null
+     */
+}
